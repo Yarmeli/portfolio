5bc9587015e71c7633dd48ad1f960ad960748dcf
Yarmeli <yarmelimine@gmail.com>
2024-12-07 04:27:02 +0000

    adjust image remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: process.env.PAYLOAD_PUBLIC_SERVER_URL.replace("https://", ""),
+      },
+      {
+        hostname: "localhost",
+      },
+    ],
+  },
 };
 
 export default withPayload(nextConfig);
