41a1590a326b1b4987264ca6729f0586af5f1386
Yarmeli <yarmelimine@gmail.com>
2024-12-07 23:47:30 +0000

    fix order by for skills

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -13,7 +13,7 @@ async function getHomeData() {
   const [aboutData, experiencesData, skillsData, projectsData] = await Promise.all([
     payload.find({ collection: "about", limit: 1 }),
     payload.find({ collection: "experience", sort: "-startDate" }),
-    payload.find({ collection: "skills", sort: "-createdAt" }),
+    payload.find({ collection: "skills", sort: "+updatedAt" }),
     payload.find({
       collection: "projects",
       sort: "-createdAt",
